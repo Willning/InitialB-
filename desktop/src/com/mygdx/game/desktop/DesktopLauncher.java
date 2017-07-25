8bc1d2cf2cbe14a370fef7c9930f75eb3c64c4a0
William Ning <12452williamning@gmail.com>
2017-07-25 17:35:50 +1200

    Started working on other screens and menus

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,15 +2,17 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+
+import gameLogic.MainGame;
+import gameLogic.GameManager;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width=720;
 		config.height=480;
-		config.backgroundFPS=60;
-		config.foregroundFPS=60;
-		new LwjglApplication(new MyGdxGame(), config);
+		config.backgroundFPS=30;
+		config.foregroundFPS=40;
+		new LwjglApplication(new GameManager(), config);
 	}
 }
